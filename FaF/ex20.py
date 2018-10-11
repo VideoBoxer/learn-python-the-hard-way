f98498902873f1206b955bbbe71be4c89f5c071b
JoseMorales7 <31421231+JoseMorales7@users.noreply.github.com>
2018-10-11 13:04:50 -0400

    Update ex20.py

--- FaF/ex20.py
+++ FaF/ex20.py
@@ -2,26 +2,26 @@ from sys import argv
 
 script, input_file = argv
 
-def print_all(f):
-    print(f.read())
+def print_all(file):
+    print(file.read())
     
-def rewind(f):
-    f.seek(0)
+def rewind(file):
+    file.seek(0)
     
     def print_a_line(line_count, f):
         print(line_count, f.readline())
         
 current_file = open(input_file)
 
-print("First let's print the whole file:\n")
+print("First let's print the whole file:")
 
 print_all(current_file)
 
-print("Now  let's rewind, kind of like a tape.")
+print("Now let's rewind, kind of like a tape.")
 
 rewind(current_file)
 
-print("Let's print three lines:")
+print("Let's print three lines: ")
 
 Current_line = 1
 print_a_line(current_line, current_file)
